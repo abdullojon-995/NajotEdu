6713e0a7fa1ef7f0c2f7b85831ce640e477d8605
Abdullojon-pro <abdullojon9526@gmail.com>
2023-11-17 03:50:18 +0500

    Added auth functionality

--- NajotTalim.Infrastructure/DependencyInjection.cs
+++ NajotTalim.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using NajotTalim.Domain.Enums;
+using NajotTalim.Infrastructure.Abstractions;
 using NajotTalim.Infrastructure.Persistence;
+using NajotTalim.Infrastructure.Services;
+using System.Text;
 
 namespace NajotTalim.Infrastructure
 {
@@ -13,6 +19,32 @@ namespace NajotTalim.Infrastructure
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<ITokenService, JWTToken>();
+            services.AddScoped<IAuthService, AuthService>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters()
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = configuration["JWT:Issuer"],
+                            ValidAudience = configuration["JWT:Audience"],
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+                        };
+                    });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminActions", policy =>
+                {
+                    policy.RequireClaim("Role", UserRole.Admin.ToString());
+                });
             });
 
             return services;
---
+++ NajotTalim.Infrastructure/Services/AuthService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NajotTalim.Infrastructure.Abstractions;
+using NajotTalim.Infrastructure.HashGenerator;
+using NajotTalim.Infrastructure.Persistence;
+
+namespace NajotTalim.Infrastructure.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ITokenService _tokenService;
+
+        public AuthService(ApplicationDbContext context,
+            ITokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+        public async Task<string> LoginAsync(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == username);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            if (user.PasswordHash != GenerateHash.GetHash(password))
+            {
+                throw new Exception("Password is wrong");
+            }
+
+            return _tokenService.GenerateAccessToken(user);
+        }
+    }
+}
